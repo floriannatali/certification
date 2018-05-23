79ea74c534d7d76f368e6f913bea36cb5e37e4b5
florian <florian.natali@france-pari.org>
2018-05-23 18:04:00 +0200

    delete useless comments

--- src/AppBundle/Controller/HttpController.php
+++ src/AppBundle/Controller/HttpController.php
@@ -13,15 +13,9 @@ class HttpController
     /**
      * @Route("/client-server-interaction")
      * @Template("@App/Http/client_server_interaction.html.twig")
-     *
-     * @throws \Twig_Error_Syntax
-     * @throws \Twig_Error_Runtime
-     * @throws \Twig_Error_Loader
-     * @throws \InvalidArgumentException
      */
-    public function php53To56Action(\Twig_Environment $twig)
+    public function php53To56Action()
     {
         return [];
     }
-
 }
